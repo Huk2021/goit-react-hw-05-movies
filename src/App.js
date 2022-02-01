4bf02989ff86b5fe09605b94f286d0ccd5290aca
Viktoriia Huk <relax16011991@gmail.com>
2022-02-01 22:05:22 +0200

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Container from './components/Container/Container';
+import AppBar from 'components/AppBar/AppBar';
+
+export default function App() {
+    return (
+        <Container>
+         <AppBar />
+        </Container>
+    );
+}
