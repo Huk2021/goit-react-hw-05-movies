ada2ae0d58f83aab6e84b032c9ca4cb1002757a9
Viktoriia Huk <relax16011991@gmail.com>
2022-02-02 00:34:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Container from './components/Container/Container';
 import AppBar from 'components/AppBar/AppBar';
+
+
+const HomePage = lazy(() => import('./components/HomePage/HomePage'));
 
 export default function App() {
     return (
         <Container>
-         <AppBar />
+          <AppBar />
+           <Suspense fallback={<h1>ЗАГРУЖАЕМ МАРШРУТ...</h1>}>
+             <Switch>
+               <Route path="/" exact>
+                  <HomePage />
+                </Route>
+            </Switch>
+           </Suspense>
         </Container>
     );
 }
