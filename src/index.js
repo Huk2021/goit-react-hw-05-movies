9db8c267c4d28bd6ef7af47c07d574da622cd040
Viktoriia Huk <relax16011991@gmail.com>
2022-02-01 22:05:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  App  from '/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import  App  from './App';
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <App />
-     </BrowserRouter>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
