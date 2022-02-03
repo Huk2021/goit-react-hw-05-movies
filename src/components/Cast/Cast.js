77155008f58c66f539eac81bd3d4c2ae5f685052
Viktoriia Huk <relax16011991@gmail.com>
2022-02-03 14:12:21 +0200

    Create Cast.js

---
+++ src/components/Cast/Cast.js
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchMovieCredits } from "../../services/api";
+
+
+const picturePath = "https://image.tmdb.org/t/p/w200";
+const pictureDefaultProfile =  "https://i.stack.imgur.com/l60Hf.png";
+
+const Cast = () => {
+  const [cast, setCast] = useState(null);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    fetchMovieCredits(movieId)
+      .then((result) => setCast([...result.cast]))
+     
+  }, []);
+
+  return (
+    <section >
+      <ul>
+        {cast &&
+          cast.map(({ id, profile_path, name, character }) => (
+            <li key={id} >
+              <img
+                src={
+                  profile_path
+                    ? `${picturePath}/${profile_path}`
+                    : pictureDefaultProfile
+                }
+                alt={`${name}`}
+                
+              ></img>
+              <p >{name}</p>
+              <p >
+                {character !== ""
+                  ? `Character: ${character}`
+                  : "Character: n/a"}
+              </p>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Cast;
+
+
